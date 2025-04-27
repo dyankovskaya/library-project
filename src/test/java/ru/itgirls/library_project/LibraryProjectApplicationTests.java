8256707d0a4e9b8098c41aac5c7477a1ce56bd0b
dyankovskaya <daryayankovskaya97@gmail.com>
2025-04-27 15:55:42 +0300

    first commit

---
+++ src/test/java/ru/itgirls/library_project/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itgirls.library_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
